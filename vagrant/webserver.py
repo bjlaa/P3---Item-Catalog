9bcb8796387e9beccdaf8f83a30ee933f1628dd4
Benjamin <benjamin.j.arias@gmail.com>
2015-12-17 20:25:37 +0100

    feat: Completed do_post method

--- vagrant/webserver.py
+++ vagrant/webserver.py
@@ -6,20 +6,23 @@ class webserverhandler(BaseHTTPRequestHandler):
 		try:
 			if self.path.endswith("/hello"):
 				self.send_response(200)
-				self.send_header('Content-type', 'text/html')
+				self.send_header('content-type', 'text/html')
 				self.end_headers()
 
 				output = ""
 				output += "<html><body>Hello!</body></html>"
+				output += "<form method='POST' enctype='multipart/form-data' action='hello'> <h2>What would you like me to say?</h2><input name='message'type='text' ><input type='submit' value='Submit'></form>"
+				output += "</body></html>"
 				self.wfile.write(output)
 
 			if self.path.endswith("/hola"):
 				self.send_response(200)
-				self.send_header('Content-type', 'text/html')
+				self.send_header('content-type', 'text/html')
 				self.end_headers()
 
 				output = ""
 				output += "<html><body>&#161Hola! <a href='/hello'>Back to hello</a></body></html>"
+				output += "<form method='POST' enctype='multipart/form-data' action='hello'> <h2>What would you like me to say?</h2><input name='message'type='text' ><input type='submit' value='Submit'></form>"
 				self.wfile.write(output)
 
 		except IOError:
@@ -33,8 +36,20 @@ class webserverhandler(BaseHTTPRequestHandler):
 			ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
 			if ctype == 'multipart/form-data':
 				fields=cgi.parse_multipart(self.rfile, pdict)
-				messageContent = fields.get('message')
+				messagecontent = fields.get('message')
+			output = ""
+			output += "<html><body>"
+			output += "<h2> Okay, how about this: </h2>"
+			output += "<h1> %s </h1>" % messagecontent[0]
+
+			
+			output += "<form method='POST' enctype='multipart/form-data' action='hello'> <h2>What would you like me to say?</h2><input name='message'type='text' ><input type='submit' value='Submit'></form>"
+			output += "</body></html>"
+			self.wfile.write(output)
+			print output
+
 		except:
+			pass
 
 def main():
 	try:
