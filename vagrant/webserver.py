0d94d29f8904379060b1ec719eff47253079ba85
Benjamin <benjamin.j.arias@gmail.com>
2015-12-18 13:01:44 +0100

    feat: Imported modules for sqlalchemy + created session

--- vagrant/webserver.py
+++ vagrant/webserver.py
@@ -1,5 +1,15 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer 
 import cgi
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Restaurant, Base, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+
+session = DBSession()
 
 class webserverhandler(BaseHTTPRequestHandler):
 	def do_GET(self):
